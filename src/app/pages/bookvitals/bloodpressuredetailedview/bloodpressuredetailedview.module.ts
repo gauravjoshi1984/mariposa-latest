7acb20e978f939ef5d845623abd52cb78bf5795d
Vaishnav <vaishnavmtr@gmail.com>
2020-08-11 10:16:07 +0530

    changes

--- src/app/pages/bookvitals/bloodpressuredetailedview/bloodpressuredetailedview.module.ts
+++ src/app/pages/bookvitals/bloodpressuredetailedview/bloodpressuredetailedview.module.ts
@@ -1,20 +1,22 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
 
-import { IonicModule } from '@ionic/angular';
+import { IonicModule } from "@ionic/angular";
 
-import { BloodpressuredetailedviewPageRoutingModule } from './bloodpressuredetailedview-routing.module';
+import { BloodpressuredetailedviewPageRoutingModule } from "./bloodpressuredetailedview-routing.module";
 
-import { BloodpressuredetailedviewPage } from './bloodpressuredetailedview.page';
+import { BloodpressuredetailedviewPage } from "./bloodpressuredetailedview.page";
+import { NgApexchartsModule } from "ng-apexcharts";
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    BloodpressuredetailedviewPageRoutingModule
+    BloodpressuredetailedviewPageRoutingModule,
+    NgApexchartsModule,
   ],
-  declarations: [BloodpressuredetailedviewPage]
+  declarations: [BloodpressuredetailedviewPage],
 })
 export class BloodpressuredetailedviewPageModule {}
--- src/app/pages/bookvitals/bloodpressuredetailedview/bloodpressuredetailedview.page.ts
+++ src/app/pages/bookvitals/bloodpressuredetailedview/bloodpressuredetailedview.page.ts
@@ -1,15 +1,90 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ChartOptions } from "../vitals/vitals.page";
 
 @Component({
-  selector: 'app-bloodpressuredetailedview',
-  templateUrl: './bloodpressuredetailedview.page.html',
-  styleUrls: ['./bloodpressuredetailedview.page.scss'],
+  selector: "app-bloodpressuredetailedview",
+  templateUrl: "./bloodpressuredetailedview.page.html",
+  styleUrls: ["./bloodpressuredetailedview.page.scss"],
 })
 export class BloodpressuredetailedviewPage implements OnInit {
+  public chartOptions: Partial<ChartOptions>;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-  }
+    this.chartOptions = {
+      series: [
+        {
+          name: "blue",
+          data: [
+            {
+              x: "S",
+              y: [70, 120],
+            },
+            {
+              x: "M",
+              y: [80, 130],
+            },
+            {
+              x: "T",
+              y: [65, 115],
+            },
+            {
+              x: "W",
+              y: [80, 120],
+            },
+            {
+              x: "T",
+              y: [85, 135],
+            },
+            {
+              x: "F",
+              y: [80, 130],
+            },
+            {
+              x: "S",
+              y: [70, 120],
+            },
+          ],
+        },
+      ],
+      yaxis: {
+        opposite: true,
 
+        labels: {
+          formatter: function (value) {
+            if (value == 60 || value == 100 || value == 140) {
+              return value + "$MMHG";
+            }
+          },
+        },
+      },
+
+      chart: {
+        type: "rangeBar",
+        height: 110,
+        width: 310,
+        toolbar: {
+          show: false,
+        },
+      },
+      plotOptions: {
+        bar: {
+          horizontal: false,
+          columnWidth: "18",
+        },
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      fill: {
+        colors: ["#2AE4F0"],
+      },
+      grid: {
+        xaxis: {
+          lines: { show: true },
+        },
+      },
+    };
+  }
 }
